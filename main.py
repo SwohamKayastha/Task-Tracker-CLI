4944b79b1973adc4ad6edfe6ba7bfca743d15d58
Swoham <swohamadditional@gmail.com>
2024-08-19 12:08:18 +0545

    added function

--- main.py
+++ main.py
@@ -21,14 +21,21 @@ def commands():
 @click.argument("filepath", type=click.Path(exists=False), required=0)
 @click.option("--name", prompt="Name", help="input the name object")
 @click.option("--to_do", prompt="To Do", help="input the to_do object")
-def item(name, to_do, filepath):
+@click.option("--progress", prompt="Progress", help="input the progress status")
+def item(name, to_do, progress, filepath):
     filepath = "store.json"
     with open(filepath, 'r') as fb:
         data = json.load(fb)
 
+    count = 1
+    for i in data["list"]:
+        count += 1
+
     new_json = {
+        "id": count,
         "name": name,
-        "to_do": to_do
+        "to_do": to_do,
+        "progress": progress
     }
 
     data["list"].append(new_json)
@@ -37,6 +44,7 @@ def item(name, to_do, filepath):
         json.dump(data, fb, indent=4, sort_keys=True)
 
 
+# Function to read whole list
 @click.command()
 @click.argument("filepath", type=click.Path(exists=False), required=0)
 def display(filepath):
@@ -47,13 +55,54 @@ def display(filepath):
 
         print("\nDisplaying The whole list of To Do :")
         for i in data["list"]:
+            print(f"ID: {i["id"]}")
             print(f"Name:{i["name"]}")
-            print(f"To Do: {i["to_do"]}\n")
+            print(f"To Do: {i["to_do"]}")
+            print(f"Progress: {i["progress"]}\n")
+
+
+# Function that displays the function that are in progress
+@click.command()
+@click.argument("filepath", type=click.Path(exists=False), required=0)
+def display_progress(filepath):
+    filepath = "store.json"
+
+    with open(filepath, 'r') as fb:
+        data = json.load(fb)
+
+        print("\nDisplaying the list of task in progress only:")
+        for i in data["list"]:
+            if i["progress"].lower() == "progress":
+                print(f"ID: {i["id"]}")
+                print(f"Name:{i["name"]}")
+                print(f"To Do: {i["to_do"]}")
+                print(f"Progress: {i["progress"]}\n")
+
+
+# Function that displays the task that are completed
+@click.command()
+@click.argument("filepath", type=click.Path(exists=False), required=0)
+def display_completed(filepath):
+    filepath = "store.json"
+
+    with open(filepath, 'r') as fb:
+        data = json.load(fb)
+
+        print("\nDisplaying the list of task in completed only:")
+        for i in data["list"]:
+            if i["progress"].lower() == "completed":
+                print(f"ID: {i["id"]}")
+                print(f"Name:{i["name"]}")
+                print(f"To Do: {i["to_do"]}")
+                print(f"Progress: {i["progress"]}\n")
+
 
 
 # Command Function
 commands.add_command(item)
 commands.add_command(display)
+commands.add_command(display_progress)
+commands.add_command(display_completed)
 
 if __name__ == "__main__":
     commands()
